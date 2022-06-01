9e06879abde2a6417e36b746b9883c0590d71af5
Todd Guerra <tguerra@adobe.com>
2022-06-01 15:43:27 -0400

    some code cleanup and added error alert for if the call craps its pants

--- dce-capstone/src/app/flow-service.service.ts
+++ dce-capstone/src/app/flow-service.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FlowServiceService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getTest(url: string, payload: any) {
-    const headers = new HttpHeaders()
-      .set('content-type', 'application/json');
-    return this.http.post(
-      url,
-      payload,
-    {observe: 'response',
-      headers: headers
-    });
-  }
+    getTest(url: string, payload: any) {
+        const headers = new HttpHeaders()
+            .set('content-type', 'application/json');
+        return this.http.post(
+            url,
+            payload,
+            {
+                observe: 'response',
+                headers: headers
+            });
+    }
 }
