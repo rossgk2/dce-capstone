f1337a07372b1ad2956c0f1e0b6c41fc4fc267d0
Todd Guerra <tguerra@adobe.com>
2022-06-01 13:08:44 -0400

    added some preliminary styles and markup so it looks almost like a real site

--- dce-capstone/src/app/app.module.ts
+++ dce-capstone/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { PracticeTestFormComponent } from './practice-test-form/practice-test-form.component';
+import {AppComponent} from './app.component';
+import {PracticeTestFormComponent} from './practice-test-form/practice-test-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxSpinnerModule} from "ngx-spinner";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PracticeTestFormComponent
-  ],
+    declarations: [
+        AppComponent,
+        PracticeTestFormComponent
+    ],
     imports: [
-      BrowserModule,
-      ReactiveFormsModule,
-      HttpClientModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
         BrowserAnimationsModule,
         NgxSpinnerModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
