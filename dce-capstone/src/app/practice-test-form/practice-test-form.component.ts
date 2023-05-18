df2f130057c730d18e00f1831ecd0f736445c14e
Ross Grogan-Kaylor <per06434@adobe.com>
2023-05-18 17:28:41 -0400

    Basic edits

--- dce-capstone/src/app/practice-test-form/practice-test-form.component.ts
+++ dce-capstone/src/app/practice-test-form/practice-test-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import questionData from 'src/assets/biology-test-sample-data.json';
 import payloadJson from 'src/assets/to-send.json';
 import {FlowServiceService} from "../flow-service.service";
@@ -12,10 +12,11 @@ import {FlowServiceService} from "../flow-service.service";
 export class PracticeTestFormComponent implements OnInit {
 
     public insuranceForm: FormGroup;
-    private questions = questionData.questions;
+    private initiatorType: 'traveler' | 'program-manager' = 'traveler';
+    
     private testPayload = payloadJson;
-
     private webFormUrl = 'https://sign-acs-todd.na4.adobesign.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhBabsyw5jvNBjA4z7Nzw-1Kq361mJmS5BqfzwmdS-EkWicnyGvKAD4audzvpsfBpWc*';
+    
 
     //pulled from the "when http request is received" step of the Power Automate flow
     private flowUrl = 'https://prod-73.westus.logic.azure.com:443/workflows/85f9a73063894f849027bb8f0370aacf/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=m1rNyUBz3_j4d_C3DqckgaIhIzZ9HXJ7sAhqJfk4IXk';
@@ -23,16 +24,20 @@ export class PracticeTestFormComponent implements OnInit {
     constructor(private formBuilder: FormBuilder,
                 private flowService: FlowServiceService) {
         this.insuranceForm = this.formBuilder.group({
-            state: ['', Validators.required],
-            numberEmployees: ['', [Validators.required]],
-            formType: ['', [Validators.required]]
+            email: ['', [Validators.required]],
+            initiatorType: this.formBuilder.group({initiatorType: ['', Validators.required]})
         });
     }
 
     onSubmit(): void {
-        if (this.insuranceForm.controls['numberEmployees'].value == 27) {
-            window.location.href = this.webFormUrl;
-        } else {
+        if (this.initiatorType === 'traveler') {
+        }
+        else { // this.initiatorType === 'program-manager'
+
+        }
+
+                    // window.location.href = this.webFormUrl;
+
             let call = this.flowService.makeAgreement(this.flowUrl, this.testPayload);
             console.log('starting call...');
             call.subscribe(data => {
@@ -47,6 +52,10 @@ export class PracticeTestFormComponent implements OnInit {
                 alert('There has been an error. How embarrassing.');
             });
         }
+    }
+
+    changeInitiatorType(e: any) {
+        this.initiatorType = e.target.value;
     }
 
     ngOnInit(): void {
