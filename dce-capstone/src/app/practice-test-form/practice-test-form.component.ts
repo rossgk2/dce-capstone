83359cf41cf2a52a8a7a7ce420516c43bfa6adaa
Ross Grogan-Kaylor <per06434@adobe.com>
2023-05-18 22:23:40 -0400

    Configured FormGroup for radio buttons

--- dce-capstone/src/app/practice-test-form/practice-test-form.component.ts
+++ dce-capstone/src/app/practice-test-form/practice-test-form.component.ts
@@ -13,49 +13,43 @@ export class PracticeTestFormComponent implements OnInit {
 
     public insuranceForm: FormGroup;
     private initiatorType: 'traveler' | 'program-manager' = 'traveler';
-    
-    private testPayload = payloadJson;
-    private webFormUrl = 'https://sign-acs-todd.na4.adobesign.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhBabsyw5jvNBjA4z7Nzw-1Kq361mJmS5BqfzwmdS-EkWicnyGvKAD4audzvpsfBpWc*';
-    
-
+        
     //pulled from the "when http request is received" step of the Power Automate flow
-    private flowUrl = 'https://prod-73.westus.logic.azure.com:443/workflows/85f9a73063894f849027bb8f0370aacf/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=m1rNyUBz3_j4d_C3DqckgaIhIzZ9HXJ7sAhqJfk4IXk';
+    private flowUrl = 'https://prod-189.westus.logic.azure.com:443/workflows/2598a68443c541f18032e2211c8fdb65/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=CGvS20Ro5hfsBweScnMgVhtBsERJgvG1dWa1J7aWzA0';
 
-    constructor(private formBuilder: FormBuilder,
-                private flowService: FlowServiceService) {
+    constructor(private formBuilder: FormBuilder, private flowService: FlowServiceService) {
         this.insuranceForm = this.formBuilder.group({
-            email: ['', [Validators.required]],
-            initiatorType: this.formBuilder.group({initiatorType: ['', Validators.required]})
+            email: ['', [Validators.required, Validators.email]],
+            'initiatorType': ['traveler']
         });
     }
 
     onSubmit(): void {
-        if (this.initiatorType === 'traveler') {
-        }
-        else { // this.initiatorType === 'program-manager'
-
-        }
-
-                    // window.location.href = this.webFormUrl;
-
-            let call = this.flowService.makeAgreement(this.flowUrl, this.testPayload);
-            console.log('starting call...');
-            call.subscribe(data => {
-                console.log(data);
-                if (data.status === 202) {
-                    //delay is handled in the Power Automate flow
-                    alert("Your Form Has Been Sent.");
-                    this.insuranceForm.reset();
-                }
-            }, error => {
-                console.log(error);
-                alert('There has been an error. How embarrassing.');
-            });
-        }
+        let call = this.flowService.makeAgreement(this.flowUrl,
+        {
+            "payload":
+            {
+                "sendAgreementTo": "rossprftadobe+signer@gmail.com",
+                "initiatorType": this.insuranceForm.controls['initiatorType'].value // is either 'traveler' or 'program-manager'
+            }
+        });
+        
+        console.log('starting call...');
+        call.subscribe(data => {
+            console.log(data);
+            if (data.status === 202) {
+                //delay is handled in the Power Automate flow
+                alert("Your Form Has Been Sent.");
+                this.insuranceForm.reset();
+            }
+        }, error => {
+            console.log(error);
+            alert('There has been an error. How embarrassing.');
+        });
     }
 
-    changeInitiatorType(e: any) {
-        this.initiatorType = e.target.value;
+    changeInitiatorType(event: any) {
+        this.insuranceForm.patchValue({ initiatorType: event.target.value });
     }
 
     ngOnInit(): void {
